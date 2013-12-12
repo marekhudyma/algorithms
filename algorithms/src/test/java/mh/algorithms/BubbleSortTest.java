6fd9f3d3771549d42751dc26b0f473860162a035
Marek <marek.hudyma@gmail.com>
2013-12-12 23:09:14 +0100

    stash

--- algorithms/src/test/java/mh/algorithms/BubbleSortTest.java
+++ algorithms/src/test/java/mh/algorithms/BubbleSortTest.java
@@ -6,5 +6,4 @@ public class BubbleSortTest extends AbstractSortTest {
         sortable = new BubbleSort();
     }
 
-
 }
---
+++ algorithms/src/test/java/mh/algorithms/MergeSortTest.java
@@ -0,0 +1,9 @@
+package mh.algorithms;
+
+public class MergeSortTest extends AbstractSortTest {
+
+    public MergeSortTest() {
+        sortable = new MergeSort();
+    }
+
+}
