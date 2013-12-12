e93878f464fb805a1b486e827331f8d2a4cb8d38
Marek Hudyma <marek.hudyma@groupon.com>
2013-12-12 18:22:40 +0100

    stash

---
+++ algorithms/src/main/java/mh/algorithms/MergeSort.java
@@ -0,0 +1,63 @@
+package mh.algorithms;
+
+/**
+ * User: mhudyma
+ */
+public class MergeSort implements Sortable {
+
+
+    @Override
+    public void sort(Comparable[] array) {
+        mergesort(0, array.length - 1);
+    }
+
+    private void mergesort(Comparable[] array, int low, int high) {
+        // check if low is smaller then high, if not then the array is sorted
+        if (low < high) {
+            // Get the index of the element which is in the middle
+            int middle = low + (high - low) / 2;
+            // Sort the left side of the array
+            mergesort(array, low, middle);
+            // Sort the right side of the array
+            mergesort(array, middle + 1, high);
+            // Combine them both
+            merge(low, middle, high);
+        }
+    }
+
+    private void merge(int low, int middle, int high) {
+
+        // Copy both parts into the helper array
+        for (int i = low; i <= high; i++) {
+            helper[i] = numbers[i];
+        }
+
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+        // Copy the smallest values from either the left or the right side back
+        // to the original array
+        while (i <= middle && j <= high) {
+            if (helper[i] <= helper[j]) {
+                numbers[k] = helper[i];
+                i++;
+            } else {
+                numbers[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+        // Copy the rest of the left side of the array into the target array
+        while (i <= middle) {
+            numbers[k] = helper[i];
+            k++;
+            i++;
+        }
+
+    }
+}
+}
+
+
+
+
