2d049dde457549197acd7aa1e51ddfeb3b666cf3
Marek Hudyma <marek.hudyma@groupon.com>
2014-01-03 17:06:31 +0100

    mistake in comment

--- algorithms/src/main/java/mh/algorithms/MergeSort.java
+++ algorithms/src/main/java/mh/algorithms/MergeSort.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  *
  * Mergesort is a divide and conquer algorithm
  *
- * complexity O(n^2)
+ * complexity O(n * log n)
  * memory 2xO(1)
  */
 public class MergeSort implements Sortable {
