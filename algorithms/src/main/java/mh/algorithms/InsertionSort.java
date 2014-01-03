ace6ef1fd65d7437e3bda193b41793ee1d22701c
Marek Hudyma <marek.hudyma@groupon.com>
2014-01-03 12:04:08 +0100

    insertion sort

--- algorithms/src/main/java/mh/algorithms/InsertionSort.java
+++ algorithms/src/main/java/mh/algorithms/InsertionSort.java
@@ -1,36 +1,59 @@
 package mh.algorithms;
 
+/**
+ * http://en.wikipedia.org/wiki/Insertion_sort
+ *
+ * a simple sorting algorithm that builds the final sorted array (or list) one item at a time
+ *
+ * complexity O(x^2)
+ * memory O(1)
+ *
+ * iterating from right side to left and insert element if order is wrong
+ */
 public class InsertionSort implements Sortable {
 
     @Override
     public void sort(Comparable[] array) {
 
-        for(int j=0; j<array.length; j++) {
-            Comparable maxValue = array[0];
-            for(int i=0; i<array.length; i++) {
-                if(array[i].compareTo(maxValue) < 0) {
-                    Comparable tmp = array[i];
-                    int index = indexOfFirtElementSmallerThan(array, tmp);
-                    moveArrayOneRight(array, index, i);
-                    array[index] = tmp;
-                }
+        // iterate through array
+        for(int j=array.length-2; j>=0; j--) {
+            Comparable currentElement = array[j];
+            int index = indexOfElementSmallerThanValueFromRightSide(array, j, currentElement);
+            if(index > 0) {
+                moveArrayOneLeft(array, j+1, index);
+                array[index] = currentElement;
             }
         }
-
+        System.out.println("");
     }
 
-    private int indexOfFirtElementSmallerThan(Comparable[] array, Comparable value) {
-        for(int i=0; i<array.length; i++) {
-            if(array[i].compareTo(value) > 0) {
+    /**
+     * method iterate from end of array (included) to indexFrom (included)
+     * and return index of first smaller element
+     * or return -1 if not found
+     * @param array
+     * @param indexFrom
+     * @param value
+     * @return index of smaller element from the right side or -1
+     */
+    private int indexOfElementSmallerThanValueFromRightSide(Comparable[] array, int indexFrom, Comparable value) {
+        for(int i=array.length-1; i>=indexFrom; i--) {
+            if(array[i].compareTo(value) < 0) {
                 return i;
             }
         }
-        return array.length;
+        return -1;
     }
 
-    private void moveArrayOneRight(Comparable[] array, int indexStart, int indexEnd) {
-        for(int i=indexEnd; i>indexStart; i--){
-             array[i] = array[i-1];
+    /**
+     * move array elements one left from indexStart (included) to indexEnd (included)
+     * @param array
+     * @param indexStart
+     * @param indexEnd
+     */
+    private void moveArrayOneLeft(Comparable[] array, int indexStart, int indexEnd) {
+        for(int i=indexStart; i<=indexEnd; i++){
+             array[i-1] = array[i];
         }
     }
 
